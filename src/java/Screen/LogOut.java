7bb6c55ba947ba8cfee9e7de4fb5ab1012f0e346
SonBui <sonbuilethai@gmail.com>
2024-07-01 15:00:06 +0700

    Just Log Out

---
+++ src/java/Screen/LogOut.java
@@ -0,0 +1,35 @@
+package Screen;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class LogOut extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        HttpSession sesh=request.getSession();
+        sesh.removeAttribute("loggedinuser");
+        request.getRequestDispatcher("index.jsp").forward(request,response);
+    } 
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
