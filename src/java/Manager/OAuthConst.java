a7e0fe99db73c0cae8073b15a3ef0e784411183c
Bui Le Thai Son <117760720+WilliamBC24@users.noreply.github.com>
2024-08-30 16:56:47 +0700

    Update OAuthConst.java

--- src/java/Manager/OAuthConst.java
+++ src/java/Manager/OAuthConst.java
@@ -2,7 +2,7 @@ package Manager;
 
 public class OAuthConst {
     public static String CLIENT_ID="906858915468-a8dshf351e88vccqkv3qdf2m3kt5pme1.apps.googleusercontent.com";
-    public static String CLIENT_SECRET="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";
+    public static String CLIENT_SECRET="xxx";
     public static String REDIRECT_URI="http://localhost:8080/stbcStore/OAuth";
     public static String GET_TOKEN="https://oauth2.googleapis.com/token";
     public static String GET_USER_INFO="https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
